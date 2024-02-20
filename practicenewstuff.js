840cdf2014d22b971b591494b1d8bc42b7b5a8de
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-02-20 06:30:10 +0200

    Add files via upload

---
+++ practicenewstuff.js
@@ -0,0 +1,24 @@
+hello = function() {
+    return "Hello World!";
+}
+
+hello = () => {
+    return "Hello World!";
+}
+
+hello = () => "Hello World!";
+
+element.classList.add("pen", "pencil", "book");
+element.classList.remove();
+let numb = element.classList.length;
+
+
+
+
+
+
+
+
+
+
+
